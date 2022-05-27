dc92f165cbbf7fef8e5eb888b4cfe85c8974db4b
Álvaro <iespalvarobueno1999@hotmail.com>
2022-05-27 13:17:25 +0200

    prueba jsonld

--- js/details.js
+++ js/details.js
@@ -23,6 +23,8 @@ function generateJSONld(monumento){
             "name":monumento.name,
             "identifier": monumento.identifier,
             "image": monumento.image[0],
+            "image": monumento.image[1],
+            "image": monumento.image[2],
             "description": monumento.description,
             "geo": {
               "@type": "GeoCoordinates",
